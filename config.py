3a15756216abad7a43813b52c5753801d5c0c6a8
Titus Tzeng <titusjgr@gmail.com>
2019-05-29 12:37:05 +0800

    Save constants in config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+SCREEN_SIZE = 320, 320
+MAP_SIZE = 32, 32  # This is the map for snake coordination.
+SNAKE_SIZE = 10  # This indicates how big a block of the snake looks.
+INITIAL_LENGTH = 4
+FPS = 15
+NUM_FRAMES = 4  # num of frames used as input state at a time
