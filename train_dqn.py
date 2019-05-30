01061ec366abedaaa93f9469cd53be7a2b26bcac
Titus Tzeng <titusjgr@gmail.com>
2019-05-30 20:18:06 +0800

    Remove try expression and added stride

--- train_dqn.py
+++ train_dqn.py
@@ -65,7 +65,7 @@ MEMORY_CAPACITY = 4000
 
 def build_q_network():
     model = Sequential()
-    model.add(Conv2D(16, (4, 4), activation='elu',
+    model.add(Conv2D(16, (4, 4), strides=(2, 2), activation='elu',
                      input_shape=INPUT_SHAPE))
     model.add(Conv2D(32, (2, 2), activation='elu'))
     model.add(Conv2D(32, (2, 2), activation='elu'))
@@ -167,45 +167,42 @@ main_q_network.compile(
 loss_history = []
 q_history = []
 
-try:
-    for episode in range(EPISODES):
-        env = Environment()
-        state = env.positions_to_image()
-        done = False
-        game_steps = 0
-        while not done:
-            # Choose an action and transform to the next state
-            action = epsilon_greedy(EPSILON_START, EPSILON_END, step, state)
-            next_state, reward, done = env.step(action)
-            replaymemory.push(state, action, reward, next_state, done)
 
-            # Update parameters
-            loss_history.append(update_parameters())
+for episode in range(EPISODES):
+    env = Environment()
+    state = env.positions_to_image()
+    done = False
+    game_steps = 0
+    while not done:
+        # Choose an action and transform to the next state
+        action = epsilon_greedy(EPSILON_START, EPSILON_END, step, state)
+        next_state, reward, done = env.step(action)
+        replaymemory.push(state, action, reward, next_state, done)
 
-            # Periodically copy weights to target network
-            if step % COPY_STEPS == 0:
-                target_q_network.set_weights(main_q_network.get_weights())
-                q_history.append(
-                    mean(target_q_network.predict(state[newaxis, :])))
+        # Update parameters
+        loss_history.append(update_parameters())
 
-            step += 1
-            game_steps += 1
+        # Periodically copy weights to target network
+        if step % COPY_STEPS == 0:
+            target_q_network.set_weights(main_q_network.get_weights())
+            q_history.append(
+                mean(target_q_network.predict(state[newaxis, :])))
 
-        print('Score:', env.snake.score, 'Episode:', episode)
-        checkpoint_filename = 'ckpt/num{}-steps{}-score{}.h5'.format(
-            len(listdir('ckpt')), game_steps, env.snake.score)
-        main_q_network.save_weights(checkpoint_filename)
+        step += 1
+        game_steps += 1
 
-except:
-    pass
-finally:
-    print('Time used:', time.time() - start_time)
+    print('Score:', env.snake.score, 'Episode:', episode)
+    checkpoint_filename = 'ckpt/num{}-steps{}-score{}.h5'.format(
+        len(listdir('ckpt')), game_steps, env.snake.score)
+    main_q_network.save_weights(checkpoint_filename)
 
-    subplot(211)
-    plot(loss_history)
-    title('loss')
-    subplot(212)
-    plot(q_history)
-    title('q value')
-    savefig('loss-plot/' + str(int(time.time())) +
-            '-lr' + LEARNING_RATE + '.png')
+print('Time used:', time.time() - start_time)
+
+subplot(211)
+plot(loss_history)
+title('loss')
+subplot(212)
+plot(q_history)
+title('q value')
+savefig('loss-plot/' + str(int(time.time())) +
+        '-lr' + LEARNING_RATE + '.png')
